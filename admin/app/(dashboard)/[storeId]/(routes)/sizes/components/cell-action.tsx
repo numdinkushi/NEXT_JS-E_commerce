6c91589ad5bf3b5a0166378a5eccadb4e395a529
numdinkushi <numdinkushi@gmail.com>
2023-07-20 09:18:50 +0100

    added sizes crud

---
+++ admin/app/(dashboard)/[storeId]/(routes)/sizes/components/cell-action.tsx
@@ -0,0 +1,80 @@
+'use client';
+
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel } from "@/components/ui/dropdown-menu";
+import { SizeColumn } from "./columns";
+import { DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu";
+import { Copy, Edit, MoreHorizontal, Trash } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { toast } from "react-hot-toast";
+import { useParams, useRouter } from "next/navigation";
+import { useState } from "react";
+import axios from "axios";
+import { AlertModal } from "@/components/modals/alert-modal";
+
+interface cellActionProps {
+    data: SizeColumn;
+}
+export const CellAction: React.FC<cellActionProps> = ({
+    data
+}) => {
+    const router = useRouter();
+    const params = useParams();
+
+    const [loading, setLoading] = useState(false);
+    const [open, setOpen] = useState(false);
+
+    const onCopy = (id: string) => {
+        navigator.clipboard.writeText(id);
+        toast.success('Size Id copied to clipboard.');
+    };
+
+    const onConfirm = async () => {
+        try {
+          setLoading(true);
+          await axios.delete(`/api/${params.storeId}/sizes/${data.id}`);
+          toast.success('Size deleted.');
+          router.refresh();
+        } catch (error) {
+          toast.error('Make sure you removed all products using this size first.');
+        } finally {
+          setOpen(false);
+          setLoading(false);
+        }
+      };
+
+    return (
+        <>
+        <AlertModal 
+            isOpen={open}
+            onClose={() => setOpen(false)}
+            onConfirm={onConfirm}
+            loading={loading}
+        />
+        <DropdownMenu >
+            <DropdownMenuTrigger asChild>
+                <Button variant="ghost" className="h-8 w-8 p-0">
+                    <span className="sr-only">Open Menu</span>
+                    <MoreHorizontal className="h-4 w-4" />
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end" >
+                <DropdownMenuLabel>
+                    Actions
+                </DropdownMenuLabel>
+                <DropdownMenuItem onClick={() => onCopy(data.id)}>
+                    <Copy className="mr-2 h-4 w-4" />
+                    Copy Id
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => router.push(`/${params.storeId}/sizes/${data.id}`)}>
+                    <Edit className="mr-2 h-4 w-4" />
+                    Update
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => setOpen(true)}>
+                    <Trash className="mr-2 h-4 w-4" />
+                    Delete
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+        </>
+    );
+};
---
+++ admin/app/(dashboard)/[storeId]/(routes)/sizes/components/client.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import { Button } from "@/components/ui/button";
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { Plus } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+import { SizeColumn, columns } from "./columns";
+import { DataTable } from "@/components/ui/data-table";
+import { ApiList } from "@/components/ui/api-list";
+
+interface SizesClientClientProps {
+    data: SizeColumn[];
+}
+
+export const SizesClient: React.FC<SizesClientClientProps> = ({ data }) => {
+    const router = useRouter();
+    const params = useParams();
+    return (
+        <>
+            <div className="flex items-center justify-between">
+                <Heading
+                    title={`Sizes (${data.length})`}
+                    description="Manage sizes for your store"
+                />
+                <Button onClick={() => router.push(`/${params.storeId}/sizes/new`)}>
+                    <Plus className="mr-2 h-4 w-4" />
+                    Add New
+                </Button>
+            </div>
+            <Separator />
+            <DataTable columns={columns} data={data} searchKey="name" />
+            <Heading title="API" description="API for Sizes" />
+            <Separator />
+            <ApiList entityIdName="sizesId" entityName="sizes" />
+        </>
+    );
+};
