e49dbcf06752c608caf6b8a6dc33bf73ce37a5f3
numdinkushi <numdinkushi@gmail.com>
2023-07-23 23:12:15 +0100

    rebased

--- store_front_End/components/ui/button.tsx
+++ store_front_End/components/ui/button.tsx
@@ -17,6 +17,7 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(({
         className={cn(`w-auto rounded-full bg-black border-transparent px-5 py-3 disabled:cursor-not-allowed disabled:opacity-50 text-white font-semibold hover:opacity-75 transition`, className)}
             ref={ref}
             {...props}
+            disabled
         >
             {children}
         </button>
